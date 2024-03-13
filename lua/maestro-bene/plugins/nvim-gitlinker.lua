080e85f87bf1a13f5d6c070014131c8726292961
maestro-bene <gael.cudennec@gmail.com>
2024-03-13 16:22:26 +0100

    docs(git-linker): which key doc

--- lua/maestro-bene/plugins/nvim-gitlinker.lua
+++ lua/maestro-bene/plugins/nvim-gitlinker.lua
@@ -6,23 +6,36 @@ return {
 			"n",
 			"<leader>gb",
 			'<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
-			{ silent = true }
+			{ silent = true, desc = "Go to Git URL for this buffer" }
 		)
 		vim.api.nvim_set_keymap(
 			"v",
 			"<leader>gb",
 			'<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
-			{}
+			{ desc = "Go to Git URL for the selection" }
 		)
-		vim.api.nvim_set_keymap("n", "<leader>gY", '<cmd>lua require"gitlinker".get_repo_url()<cr>', { silent = true })
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>gY",
+			'<cmd>lua require"gitlinker".get_repo_url()<cr>',
+			{ silent = true, desc = "Copy Git URL of the repository" }
+		)
 		vim.api.nvim_set_keymap(
 			"n",
 			"<leader>gB",
 			'<cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".open_in_browser})<cr>',
-			{ silent = true }
+			{ silent = true, desc = "Go to Git URL for this repository" }
 		)
 		require("gitlinker").setup({
-			mappings = "<leader>gy",
+			mappings = nil,
 		})
+
+		-- Creating this keymap after the setup in order to override default git linker copy line link
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>gy",
+			'<cmd>lua require"gitlinker".get_buf_range_url()<cr>',
+			{ silent = true, desc = "Copy Git URL for this buffer" }
+		)
 	end,
 }
