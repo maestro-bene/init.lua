19c5d9dbf61c9bddd4ba795f61ffc618f69d8fb3
maestro-bene <gael.cudennec@gmail.com>
2024-09-03 18:23:21 +0200

    docs(keymaps): redone keymaps following suggestions

--- lua/maestro-bene/plugins/chatgpt.lua
+++ lua/maestro-bene/plugins/chatgpt.lua
@@ -35,28 +35,28 @@ return {
 
 	keys = function()
 		local wk = require("which-key")
-		wk.register({
-			["<leader>c"] = {
-				name = "ChatGPT",
-				c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
-				p = { "<cmd>ChatGPTCompleteCode<CR>", "Complete Code" },
-				e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
-				-- All commented lines are now handled through Telescope GPT extension
-				-- g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
-				-- t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
-				k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
-				-- d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
-				-- a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
-				-- o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
-				-- s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
-				-- f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
-				-- x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
-				r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
-				l = {
+		wk.add({
+			{ "<leader>c", group = "ChatGPT" },
+			{ "<leader>cc", "<cmd>ChatGPT<CR>", desc = "ChatGPT" },
+			{ "<leader>cp", "<cmd>ChatGPTCompleteCode<CR>", desc = "Complete Code" },
+			{
+				mode = { "n", "v" },
+				{ "<leader>ce", "<cmd>ChatGPTEditWithInstruction<CR>", desc = "Edit with instruction" },
+				{ "<leader>ck", "<cmd>ChatGPTRun keywords<CR>", desc = "Keywords" },
+				{
+					"<leader>cl",
 					"<cmd>ChatGPTRun code_readability_analysis<CR>",
-					"Code Readability Analysis",
-					mode = { "n", "v" },
+					desc = "Code Readability Analysis",
 				},
+				{ "<leader>cr", "<cmd>ChatGPTRun roxygen_edit<CR>", desc = "Roxygen Edit" },
+				-- { "<leader>cg", "<cmd>ChatGPTRun grammar_correction<CR>", desc = "Grammar Correction" },
+				-- { "<leader>ct", "<cmd>ChatGPTRun translate<CR>", desc = "Translate" },
+				-- { "<leader>cd", "<cmd>ChatGPTRun docstring<CR>", desc = "Docstring" },
+				-- { "<leader>ca", "<cmd>ChatGPTRun add_tests<CR>", desc = "Add Tests" },
+				-- { "<leader>co", "<cmd>ChatGPTRun optimize_code<CR>", desc = "Optimize Code" },
+				-- { "<leader>cs", "<cmd>ChatGPTRun summarize<CR>", desc = "Summarize" },
+				-- { "<leader>cf", "<cmd>ChatGPTRun fix_bugs<CR>", desc = "Fix Bugs" },
+				-- { "<leader>cx", "<cmd>ChatGPTRun explain_code<CR>", desc = "Explain Code" },
 			},
 		})
 	end,
