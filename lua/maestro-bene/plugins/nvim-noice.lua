cf9f204b791b68800a2d4c6af9d3423fd98cdbe3
maestro-bene <gael.cudennec@gmail.com>
2024-09-03 18:22:32 +0200

    feat(ice): added support for last bits

--- lua/maestro-bene/plugins/nvim-noice.lua
+++ lua/maestro-bene/plugins/nvim-noice.lua
@@ -2,6 +2,14 @@ return {
 	"folke/noice.nvim",
 	config = function()
 		require("noice").setup({
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
 			-- add any options here
 			routes = {
 				{
