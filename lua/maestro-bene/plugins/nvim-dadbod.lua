e30abf6c2a0042a96e3a0a1f31bae06bbceb1701
maestro-bene <75665296+maestro-bene@users.noreply.github.com>
2026-04-27 14:33:35 +0200

    refactor: dadbod

--- lua/maestro-bene/plugins/nvim-dadbod.lua
+++ lua/maestro-bene/plugins/nvim-dadbod.lua
@@ -6,6 +6,29 @@ return {
 		"kristijanhusak/vim-dadbod-completion",
 	},
 	config = function()
-		require("config.dadbod").setup()
+		-- Sauvegarde des UI files dans: <stdpath("config")>/db_ui
+		vim.g.db_ui_save_location = vim.fn.stdpath("config") .. require("plenary.path").path.sep .. "db_ui"
+
+		-- Omni completion pour dadbod (fallback si pas de nvim-cmp)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "sql" },
+			command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+		})
+
+		-- Source nvim-cmp spécifique pour les buffers SQL
+		local function db_completion()
+			local ok, cmp = pcall(require, "cmp")
+			if not ok then
+				return
+			end
+			cmp.setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+		end
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "sql", "mysql", "plsql" },
+			callback = function()
+				vim.schedule(db_completion)
+			end,
+		})
 	end,
 }
