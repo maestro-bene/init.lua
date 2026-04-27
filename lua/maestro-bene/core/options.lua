33b2cb156a8997936b2c5296d1e28a26ffaf892a
maestro-bene <75665296+maestro-bene@users.noreply.github.com>
2026-04-27 14:29:26 +0200

    style: cleaning in higher files

--- lua/maestro-bene/core/options.lua
+++ lua/maestro-bene/core/options.lua
@@ -1,24 +1,23 @@
 local opt = vim.opt -- for conciseness
 
--- line numbers
+-- Line numbers
 opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
--- tabs & indentation
-opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
+-- Tabs & indentation
+opt.tabstop = 4
 opt.shiftwidth = 4 -- 4 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 opt.smartindent = true -- does the same, except it takes the syntax into account
 
--- line wrapping
---opt.wrap = false -- disable line wrapping
+-- Line wrapping
+-- opt.wrap = false -- disable line wrapping
 
--- search settings
+-- Search settings
 opt.ignorecase = false -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
--- old primeagen setting
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
 vim.opt.undofile = true
@@ -26,23 +25,20 @@ vim.opt.undofile = true
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
 
--- appearance
-
 -- turn on termguicolors for nightfly colorscheme to work
--- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
--- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+-- Allow backspace on indent, end of line or insert mode start position
+opt.backspace = "indent,eol,start"
 
--- clipboard
---opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+-- Use system clipboard as default register
+-- opt.clipboard:append("unnamedplus")
 
--- split windows
+-- Split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
--- turn off swapfile
+-- Turn off swapfile
 opt.swapfile = false
