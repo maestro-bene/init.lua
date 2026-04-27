93a29fc068ad13fdc17bfa95a297019461724bda
maestro-bene <75665296+maestro-bene@users.noreply.github.com>
2026-04-27 14:32:19 +0200

    idk

--- lua/maestro-bene/plugins/extra/presence.lua
+++ lua/maestro-bene/plugins/extra/presence.lua
@@ -1,5 +1,5 @@
 return {
-	"andweeb/presence.nvim",
-	event = "VeryLazy",
-	enabled = false, -- Experiencing insane slowness since I've been using BetterDiscord
+    "andweeb/presence.nvim",
+    event = "VeryLazy",
+    enabled = false, -- Experiencing insane slowness since I've been using BetterDiscord
 }
