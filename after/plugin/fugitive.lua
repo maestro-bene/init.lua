6c21f63840a2b4b1e8bae78fc8bab16bad37ee59
maestro-bene <75665296+maestro-bene@users.noreply.github.com>
2026-04-27 14:28:48 +0200

    Added config to lsp

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -12,7 +12,7 @@ autocmd("BufWinEnter", {
         end
 
         local bufnr = vim.api.nvim_get_current_buf()
-        local opts = {buffer = bufnr, remap = false}
+        local opts = { buffer = bufnr, remap = false }
         vim.keymap.set("n", "<leader>p", function()
             vim.cmd.Git('push')
         end, opts)
