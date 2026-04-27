a13015f137ee44fbe2353032e29318518c6b54c9
maestro-bene <75665296+maestro-bene@users.noreply.github.com>
2026-04-27 14:30:27 +0200

    feat(plugins): gitLinker added for easy communications

---
+++ lua/maestro-bene/plugins/nvim-gitlinker.lua
@@ -0,0 +1,28 @@
+return {
+	"ruifm/gitlinker.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>gb",
+			'<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+			{ silent = true }
+		)
+		vim.api.nvim_set_keymap(
+			"v",
+			"<leader>gb",
+			'<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+			{}
+		)
+		vim.api.nvim_set_keymap("n", "<leader>gY", '<cmd>lua require"gitlinker".get_repo_url()<cr>', { silent = true })
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>gB",
+			'<cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+			{ silent = true }
+		)
+		require("gitlinker").setup({
+			mappings = "<leader>gy",
+		})
+	end,
+}
