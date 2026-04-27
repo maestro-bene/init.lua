7f7489f29d064f3f782b59a1526da85ad86ae187
maestro-bene <75665296+maestro-bene@users.noreply.github.com>
2026-04-27 14:31:56 +0200

    docs(keymaps): keymaps description

--- lua/maestro-bene/plugins/gitlab-nvim.lua
+++ lua/maestro-bene/plugins/gitlab-nvim.lua
@@ -47,25 +47,30 @@ return {
 			--  url = ssh://git@<gitURL>/namespace/project.git
 		})
 
-		vim.keymap.set("n", "glr", gitlab.review)
-		vim.keymap.set("n", "gls", gitlab.summary)
-		vim.keymap.set("n", "glA", gitlab.approve)
-		vim.keymap.set("n", "glR", gitlab.revoke)
-		vim.keymap.set("n", "glc", gitlab.create_comment)
-		vim.keymap.set("v", "glc", gitlab.create_multiline_comment)
-		vim.keymap.set("v", "glC", gitlab.create_comment_suggestion)
-		vim.keymap.set("n", "glO", gitlab.create_mr)
-		vim.keymap.set("n", "glm", gitlab.move_to_discussion_tree_from_diagnostic)
-		vim.keymap.set("n", "gln", gitlab.create_note)
-		vim.keymap.set("n", "gld", gitlab.toggle_discussions)
-		vim.keymap.set("n", "glaa", gitlab.add_assignee)
-		vim.keymap.set("n", "glad", gitlab.delete_assignee)
-		vim.keymap.set("n", "glla", gitlab.add_label)
-		vim.keymap.set("n", "glld", gitlab.delete_label)
-		vim.keymap.set("n", "glra", gitlab.add_reviewer)
-		vim.keymap.set("n", "glrd", gitlab.delete_reviewer)
-		vim.keymap.set("n", "glp", gitlab.pipeline)
-		vim.keymap.set("n", "glo", gitlab.open_in_browser)
-		vim.keymap.set("n", "glM", gitlab.merge)
+		vim.keymap.set("n", "glr", gitlab.review, { desc = "Review" })
+		vim.keymap.set("n", "gls", gitlab.summary, { desc = "Summary" })
+		vim.keymap.set("n", "glA", gitlab.approve, { desc = "Approve" })
+		vim.keymap.set("n", "glR", gitlab.revoke, { desc = "Revoke" })
+		vim.keymap.set("n", "glc", gitlab.create_comment, { desc = "Create Comment" })
+		vim.keymap.set("v", "glc", gitlab.create_multiline_comment, { desc = "Create Multiline Comment" })
+		vim.keymap.set("v", "glC", gitlab.create_comment_suggestion, { desc = "Create Comment Suggestion" })
+		vim.keymap.set("n", "glO", gitlab.create_mr, { desc = "Create Merge Request" })
+		vim.keymap.set(
+			"n",
+			"glm",
+			gitlab.move_to_discussion_tree_from_diagnostic,
+			{ desc = "Move to Discussion Tree from Diagnostic" }
+		)
+		vim.keymap.set("n", "gln", gitlab.create_note, { desc = "Create Note" })
+		vim.keymap.set("n", "gld", gitlab.toggle_discussions, { desc = "Toggle Discussions" })
+		vim.keymap.set("n", "glaa", gitlab.add_assignee, { desc = "Add Assignee" })
+		vim.keymap.set("n", "glad", gitlab.delete_assignee, { desc = "Delete Assignee" })
+		vim.keymap.set("n", "glla", gitlab.add_label, { desc = "Add Label" })
+		vim.keymap.set("n", "glld", gitlab.delete_label, { desc = "Delete Label" })
+		vim.keymap.set("n", "glra", gitlab.add_reviewer, { desc = "Add Reviewer" })
+		vim.keymap.set("n", "glrd", gitlab.delete_reviewer, { desc = "Delete Reviewer" })
+		vim.keymap.set("n", "glp", gitlab.pipeline, { desc = "Pipeline" })
+		vim.keymap.set("n", "glo", gitlab.open_in_browser, { desc = "Open in Browser" })
+		vim.keymap.set("n", "glM", gitlab.merge, { desc = "Merge" })
 	end,
 }
