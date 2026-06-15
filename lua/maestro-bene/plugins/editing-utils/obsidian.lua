50ff09cdb9178cef72d021b0d00c0c82ebaba0b9
maestro-bene <75665296+maestro-bene@users.noreply.github.com>
2026-06-15 11:58:30 +0200

    fixed obsidian vault
    (cherry picked from commit 211de6fe972874d658018e0eed1d8964a098be84)

--- lua/maestro-bene/plugins/editing-utils/obsidian.lua
+++ lua/maestro-bene/plugins/editing-utils/obsidian.lua
@@ -1,51 +1,51 @@
 return {
-	"obsidian-nvim/obsidian.nvim",
-	version = "*",
-	ft = "markdown",
-	opts = {
-		legacy_commands = false,
-		workspaces = {
-			{
-				name = "Notes",
-				path = "/mnt/c/Users/WHJC4315/Documents/Notes/",
-			},
-		},
-		templates = {
-			folder = "Templates",
-			date_format = "%Y-%m-%d-%a",
-			time_format = "%H:%M",
-			customizations = {
-				meeting = {
-					notes_subdir = "Meetings",
-					note_id_func = function(title)
-						--local date = os.date("%d-%m-%Y")
-						title = title or "meeting"
-						local clean = title:gsub('[%c%p/\\:*?"<>|]', ""):gsub("%s+", " "):gsub("^%s*(.-)%s*$", "%1")
-						return string.format("%s", clean)
-					end,
-				},
-				daily = {
-					notes_subdir = "Journal",
-					note_id_func = function(_)
-						local days = {
-							Sunday = "dimanche",
-							Monday = "lundi",
-							Tuesday = "mardi",
-							Wednesday = "mercredi",
-							Thursday = "jeudi",
-							Friday = "vendredi",
-							Saturday = "samedi",
-						}
-						local date = os.date("%d-%m-%Y")
-						local day_en = os.date("%A")
-						local day = days[day_en] or day_en
-						return string.format("%s-%s", date, day)
-					end,
-				},
-			},
-		},
-		ui = {
-			enable = false,
-		},
-	},
+  "obsidian-nvim/obsidian.nvim",
+  version = "*",
+  ft = "markdown",
+  opts = {
+    legacy_commands = false,
+    workspaces = {
+      {
+        name = "Notes",
+        path = "/mnt/c/Users/WHJC4315/SharingSpace/Notes/",
+      },
+    },
+    templates = {
+      folder = "Templates",
+      date_format = "%Y-%m-%d-%a",
+      time_format = "%H:%M",
+      customizations = {
+        meeting = {
+          notes_subdir = "Meetings",
+          note_id_func = function(title)
+            --local date = os.date("%d-%m-%Y")
+            title = title or "meeting"
+            local clean = title:gsub('[%c%p/\\:*?"<>|]', ""):gsub("%s+", " "):gsub("^%s*(.-)%s*$", "%1")
+            return string.format("%s", clean)
+          end,
+        },
+        daily = {
+          notes_subdir = "Journal",
+          note_id_func = function(_)
+            local days = {
+              Sunday = "dimanche",
+              Monday = "lundi",
+              Tuesday = "mardi",
+              Wednesday = "mercredi",
+              Thursday = "jeudi",
+              Friday = "vendredi",
+              Saturday = "samedi",
+            }
+            local date = os.date("%d-%m-%Y")
+            local day_en = os.date("%A")
+            local day = days[day_en] or day_en
+            return string.format("%s-%s", date, day)
+          end,
+        },
+      },
+    },
+    ui = {
+      enable = false,
+    },
+  },
 }
